174736c7c97b05e39a8b2c1686eb543a32d80b59
Alexis Arce <arce.alexis@hotmail.com>
2021-10-03 01:04:31 -0300

    Add event handler for main nav bar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
   target: "#mainNav",
 });
+
+const navLinks = document.getElementById("navbarSupportedContent");
+navLinks.addEventListener(
+  "click",
+  () => {
+    const toggler = document.getElementById("navbar-toggler");
+    const style = window.getComputedStyle(toggler);
+    if (style.display === "block") {
+      setTimeout(() => {
+        toggler.click();
+      }, 500);
+    }
+  },
+  false
+);
