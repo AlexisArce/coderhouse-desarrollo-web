fa9d40b906381abc1e9f5109832fb1974e3837d2
Alexis Arce <arce.alexis@hotmail.com>
2021-10-04 22:06:44 -0300

    Adds content in Planes section and bootstrap tooltip component

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
-var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+// scrollSpy (bootstrap)
+let scrollSpy = new bootstrap.ScrollSpy(document.body, {
   target: "#mainNav",
 });
 
+// tooltips (bootstrap)
+let tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+// to hide navbar after clicked on link in mobile devices
 const navLinks = document.getElementById("navbarSupportedContent");
 navLinks.addEventListener(
   "click",
